efa02a81d7a887fbdd0c4c2728081ecfe5b2a6c8
ThalKod <tmarcelin@thal.tech>
2018-06-06 10:04:17 -0400

    Initial commit

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res)=>{
-    res.send("welcome there");
-});
+app.set("view engine", "ejs");
 
+app.get("/", (req, res)=>{
+    res.render("index");
+});
 
 app.listen(7000, ()=>{
     console.log("Started at " + 7000);
