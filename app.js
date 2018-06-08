989457401956b389a4635b5eced46d44a4aaa342
Ashraful Islam <ashraful-islam@users.noreply.github.com>
2018-06-09 01:13:19 +0200

    update: correct variable name from mine_type to mime_type

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const multer  = require('multer');
+const multer = require('multer');
 const File = require("./models/file");
 const Count = require("./models/count");
 const config = require("./config");
@@ -13,13 +13,13 @@ const upload = multer({
         destination: 'files/',
         filename: function (req, file, cb) {
             crypto.pseudoRandomBytes(4, function (err, raw) {
-                const mine_type = mime.lookup(file.originalname);
-                const nameSplit = file.originalname.split("."); 
-                nameSplit.pop(); 
+                const mime_type = mime.lookup(file.originalname);
+                const nameSplit = file.originalname.split(".");
+                nameSplit.pop();
                 const name = nameSplit.join(".");
-                cb(null, raw.toString('hex') + name + '.' + mime.extension(mine_type));
+                cb(null, raw.toString('hex') + name + '.' + mime.extension(mime_type));
             });
-          }
+        }
     })
 });
 
@@ -27,8 +27,8 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 // MongoDb config
-mongoose.connect(config.dbURL, (err)=>{
-    if(err){
+mongoose.connect(config.dbURL, (err) => {
+    if (err) {
         throw err;
     }
 });
@@ -36,48 +36,48 @@ mongoose.Promise = global.Promise;
 
 // seed();
 
-app.get("/", (req, res)=>{
-    File.find({}).then((rFiles)=>{
-        Count.find({}).then((rCount)=>{
-            res.render("index", {uploadTime: rFiles.length, dTime: rCount[0].count});
+app.get("/", (req, res) => {
+    File.find({}).then((rFiles) => {
+        Count.find({}).then((rCount) => {
+            res.render("index", { uploadTime: rFiles.length, dTime: rCount[0].count });
         });
-    }).catch(()=>{
+    }).catch(() => {
         res.redirect("/");
     });
 });
 
 // Ajax upload count
-app.get("/count", (req, res)=>{
-    const result = {error:"", data: ""};
+app.get("/count", (req, res) => {
+    const result = { error: "", data: "" };
 
-    File.find({}).then((rFiles)=>{
+    File.find({}).then((rFiles) => {
         result.data = rFiles.length;
         res.send(result);
-    }).catch((err)=>{
+    }).catch((err) => {
         result.error = err;
         res.send(result);
     });
 });
 
-app.get("/:id", (req, res)=>{
-    File.findOne({identifier: req.params.id}).then((rFile)=>{
+app.get("/:id", (req, res) => {
+    File.findOne({ identifier: req.params.id }).then((rFile) => {
         rFile.downloaded++;
         rFile.save();
 
-        Count.find({}).then((rCount)=>{
+        Count.find({}).then((rCount) => {
             rCount[0].count++;
             rCount[0].save();
         });
-        
+
         const file = rFile.path_on_disk;
         res.download(file);
-    }).catch(()=>{
+    }).catch(() => {
         res.redirect("/");
     });
 });
 
-app.post("/upload", upload.single("file"), (req, res)=>{
-    if(req.file){
+app.post("/upload", upload.single("file"), (req, res) => {
+    if (req.file) {
         const identifier = Math.random().toString(36).slice(2);
         const data = {
             url: identifier,
@@ -94,14 +94,14 @@ app.post("/upload", upload.single("file"), (req, res)=>{
             identifier: identifier,
         };
 
-        File.create(file).then((rFile)=>{
+        File.create(file).then((rFile) => {
             return res.status(200).send(data);
         });
     }
 });
 
 
-app.listen(process.env.PORT || 7000, ()=>{
+app.listen(process.env.PORT || 7000, () => {
     console.log("listening...");
 });
 
