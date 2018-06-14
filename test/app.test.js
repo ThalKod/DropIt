4549f2173319010971d8337327dde3dc3a589e1b
ThalKod <tmarcelin@thal.tech>
2018-06-14 11:40:57 -0400

    add GET / route unit test

--- test/app.test.js
+++ test/app.test.js
@@ -1,14 +1,24 @@
 const request = require("supertest");
 const app = require("../app");
-// const File = require("./models/file");
+const File = require("./models/file");
 const Count = require("../models/count");
 
 let count;
+let file;
 
 beforeAll(() => {
     Count.create({ count: 0 }).then((rCount)=>{
         count = rCount.count;
     });
+
+    File.create({
+        name: "testname",
+        size: 13234,
+        path_on_disk: "/test/path",
+        identifier: "2525Eda",
+    }).then((rFile)=>{
+        file = rFile;
+    })
 });
 
 describe("GET / ", ()=>{
@@ -17,5 +27,20 @@ describe("GET / ", ()=>{
             .get("/")
             .expect(200)
             .end(done);
+    });
+});
+
+describe("GET /count ", ()=>{
+    it("Should return the number of uploaded file", (done)=>{
+        request(app)
+            .get("/")
+            .expect(200)
+            .end((err, res)=>{
+                if(err){
+                    return finish(err);
+                 }
+                 console.log(res);
+                 done();
+            });
     });
 });
